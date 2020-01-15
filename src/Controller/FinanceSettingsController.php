cdcc608e126051f9d0067e5a72273bb14f39564a
Craig Rayner <craig@craigrayner.com>
2020-01-15 13:04:14 +1100

    Finance Settings Completed
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Controller/FinanceSettingsController.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * kookaburra
+ * (c) 2020 Craig Rayner <craig@craigrayner.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * User: craig
+ * Date: 15/01/2020
+ * Time: 11:25
+ */
+
+namespace Kookaburra\SchoolAdmin\Controller;
+
+use App\Container\Container;
+use App\Container\ContainerManager;
+use App\Container\Panel;
+use App\Entity\Setting;
+use App\Provider\ProviderFactory;
+use App\Util\ErrorMessageHelper;
+use Kookaburra\SchoolAdmin\Form\FinanceSettingsType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Class FinanceSettingsController
+ * @package Kookaburra\SchoolAdmin\Controller
+ */
+class FinanceSettingsController extends AbstractController
+{
+    /**
+     * settings
+     * @param Request $request
+     * @param ContainerManager $manager
+     * @param TranslatorInterface $translator
+     * @param string $tabName
+     * @return JsonResponse|Response
+     * @Route("/finance/settings/{tabName}", name="finance_settings")
+     * @IsGranted("ROLE_ROUTE")
+     */
+    public function settings(Request $request, ContainerManager $manager, TranslatorInterface $translator, string $tabName = 'General')
+    {
+        $settingProvider = ProviderFactory::create(Setting::class);
+        $settingProvider->getSettingsByScope('Finance');
+        $container = new Container();
+
+        $form = $this->createForm(FinanceSettingsType::class, null, ['action' => $this->generateUrl('school_admin__finance_settings', ['tabName' => $tabName])]);
+
+        if ($request->getContentType() === 'json') {
+            $data = [];
+            $data['status'] = 'success';
+            try {
+                $data['errors'] = $settingProvider->handleSettingsForm($form, $request, $translator);
+                if ($data['status'] === 'success')
+                    $form = $this->createForm(FinanceSettingsType::class, null, ['action' => $this->generateUrl('school_admin__finance_settings', ['tabName' => $tabName])]);
+            } catch (\Exception $e) {
+                $data = ErrorMessageHelper::getDatabaseErrorMessage($data, true);
+            }
+
+            $manager->singlePanel($form->createView());
+            $data['form'] = $manager->getFormFromContainer('formContent', 'single');
+
+            return new JsonResponse($data, 200);
+        }
+
+        $panel = new Panel('General');
+        $container->setTranslationDomain('SchoolAdmin')->addForm('General', $form->createView())->addPanel($panel)->setSelectedPanel($tabName);
+        $panel = new Panel('Invoices');
+        $container->addPanel($panel);
+        $panel = new Panel('Receipts');
+        $container->addPanel($panel);
+        $panel = new Panel('Reminders');
+        $container->addPanel($panel);
+        $panel = new Panel('Expenses');
+        $container->addPanel($panel);
+
+        // Finally Finished
+        $manager->addContainer($container)->buildContainers();
+
+        return $this->render('@KookaburraSchoolAdmin/finance/settings.html.twig');
+    }
+}
---
+++ src/Form/FinanceSettingsType.php
@@ -0,0 +1,410 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * kookaburra
+ * (c) 2020 Craig Rayner <craig@craigrayner.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * User: craig
+ * Date: 15/01/2020
+ * Time: 11:26
+ */
+
+namespace Kookaburra\SchoolAdmin\Form;
+
+use App\Entity\Setting;
+use App\Form\Type\EnumType;
+use App\Form\Type\HeaderType;
+use App\Form\Type\ReactFormType;
+use App\Form\Type\SettingsType;
+use App\Form\Type\SimpleArrayType;
+use App\Form\Type\ToggleType;
+use App\Provider\ProviderFactory;
+use Doctrine\ORM\EntityRepository;
+use Kookaburra\UserAdmin\Entity\Person;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+
+/**
+ * Class FinanceSettingsType
+ * @package Kookaburra\SchoolAdmin\Form
+ */
+class FinanceSettingsType extends AbstractType
+{
+    /**
+     * buildForm
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('generalHeader', HeaderType::class,
+                [
+                    'label' => 'General Settings',
+                    'panel' => 'General',
+                ]
+            )
+            ->add('generalSettings', SettingsType::class,
+                [
+                    'settings' => [
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'email',
+                            'entry_type' => EmailType::class,
+                            'entry_options' => [
+                                'constraints' => [
+                                    new Email(),
+                                ],
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'financeOnlinePaymentEnabled',
+                            'entry_type' => ToggleType::class,
+                            'entry_options' => [
+                                'visibleByClass' => 'financeOnlinePaymentEnabled',
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'financeOnlinePaymentThreshold',
+                            'entry_type' => TextType::class,
+                            'entry_options' => [
+                                'row_class' => 'financeOnlinePaymentEnabled flex flex-col sm:flex-row justify-between content-center p-0',
+                            ],
+                        ],
+                    ],
+                    'panel' => 'General',
+                ]
+            )
+            ->add('submit1', SubmitType::class,
+                [
+                    'label' => 'Submit',
+                    'panel' => 'General',
+                    'translation_domain' => 'messages',
+                ]
+            )
+            ->add('invoicesHeader', HeaderType::class,
+                [
+                    'label' => 'Invoices',
+                    'panel' => 'Invoices',
+                ]
+            )
+            ->add('invoicesSettings', SettingsType::class,
+                [
+                    'settings' => [
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'invoiceText',
+                            'entry_type' => TextareaType::class,
+                            'entry_options' => [
+                                'attr' => [
+                                    'rows' => 6,
+                                ],
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'invoiceNotes',
+                            'entry_type' => TextareaType::class,
+                            'entry_options' => [
+                                'attr' => [
+                                    'rows' => 6,
+                                ],
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'invoiceeNameStyle',
+                            'entry_type' => EnumType::class,
+                            'entry_options' => [
+                                'choice_list_class' => FinanceSettingsType::class,
+                                'choice_list_method' => 'getNameStyleList',
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'invoiceNumber',
+                            'entry_type' => EnumType::class,
+                            'entry_options' => [
+                                'choice_list_class' => FinanceSettingsType::class,
+                                'choice_list_method' => 'getInvoiceNumberList',
+                            ],
+                        ],
+                    ],
+                    'panel' => 'Invoices',
+                ]
+            )
+            ->add('submit2', SubmitType::class,
+                [
+                    'label' => 'Submit',
+                    'panel' => 'Invoices',
+                    'translation_domain' => 'messages',
+                ]
+            )
+            ->add('receiptHeader', HeaderType::class,
+                [
+                    'label' => 'Receipts',
+                    'panel' => 'Receipts',
+                ]
+            )
+            ->add('receiptSettings', SettingsType::class,
+                [
+                    'settings' => [
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'receiptText',
+                            'entry_type' => TextareaType::class,
+                            'entry_options' => [
+                                'attr' => [
+                                    'rows' => 6,
+                                ],
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'receiptNotes',
+                            'entry_type' => TextareaType::class,
+                            'entry_options' => [
+                                'attr' => [
+                                    'rows' => 6,
+                                ],
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'hideItemisation',
+                            'entry_type' => ToggleType::class,
+                        ],
+                    ],
+                    'panel' => 'Receipts',
+                ]
+            )
+            ->add('submit3', SubmitType::class,
+                [
+                    'label' => 'Submit',
+                    'panel' => 'Receipts',
+                    'translation_domain' => 'messages',
+                ]
+            )
+            ->add('reminderHeader', HeaderType::class,
+                [
+                    'label' => 'Reminders',
+                    'panel' => 'Reminders',
+                ]
+            )
+            ->add('reminderSettings', SettingsType::class,
+                [
+                    'settings' => [
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'reminder1Text',
+                            'entry_type' => TextareaType::class,
+                            'entry_options' => [
+                                'attr' => [
+                                    'rows' => 6,
+                                ],
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'reminder2Text',
+                            'entry_type' => TextareaType::class,
+                            'entry_options' => [
+                                'attr' => [
+                                    'rows' => 6,
+                                ],
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'reminder3Text',
+                            'entry_type' => TextareaType::class,
+                            'entry_options' => [
+                                'attr' => [
+                                    'rows' => 6,
+                                ],
+                            ],
+                        ],
+                    ],
+                    'panel' => 'Reminders',
+                ]
+            )
+            ->add('submit4', SubmitType::class,
+                [
+                    'label' => 'Submit',
+                    'panel' => 'Reminders',
+                    'translation_domain' => 'messages',
+                ]
+            )
+            ->add('expenseHeader', HeaderType::class,
+                [
+                    'label' => 'Expenses',
+                    'panel' => 'Expenses',
+                ]
+            )
+            ->add('expenseSettings', SettingsType::class,
+                [
+                    'settings' => [
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'budgetCategories',
+                            'entry_type' => SimpleArrayType::class,
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'expenseApprovalType',
+                            'entry_type' => EnumType::class,
+                            'entry_options' => [
+                                'choice_list_class' => FinanceSettingsType::class,
+                                'choice_list_method' => 'getApprovalList',
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'budgetLevelExpenseApproval',
+                            'entry_type' => ToggleType::class,
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'expenseRequestTemplate',
+                            'entry_type' => TextareaType::class,
+                            'entry_options' => [
+                                'attr' => [
+                                    'rows' => 6,
+                                ],
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'allowExpenseAdd',
+                            'entry_type' => ToggleType::class,
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'purchasingOfficer',
+                            'entry_type' => EntityType::class,
+                            'entry_options' => [
+                                'class' => Person::class,
+                                'choice_label' => 'fullNameReversed',
+                                'placeholder' => ' ',
+                                'choice_translation_domain' => false,
+                                'query_builder' => function(EntityRepository $er){
+                                    return $er->createQueryBuilder('p')
+                                        ->select(['p','s'])
+                                        ->join('p.staff', 's')
+                                        ->where('p.status = :full')
+                                        ->andWhere('s.id IS NOT NULL')
+                                        ->setParameter('full', 'Full')
+                                        ->orderBy('p.surname')
+                                        ->addOrderBy('p.firstName')
+                                        ;
+                                },
+                            ],
+                        ],
+                        [
+                            'scope' => 'Finance',
+                            'name' => 'reimbursementOfficer',
+                            'entry_type' => EntityType::class,
+                            'entry_options' => [
+                                'class' => Person::class,
+                                'choice_label' => 'fullNameReversed',
+                                'choice_translation_domain' => false,
+                                'placeholder' => ' ',
+                                'query_builder' => function(EntityRepository $er){
+                                    return $er->createQueryBuilder('p')
+                                        ->select(['p','s'])
+                                        ->join('p.staff', 's')
+                                        ->where('p.status = :full')
+                                        ->andWhere('s.id IS NOT NULL')
+                                        ->setParameter('full', 'Full')
+                                        ->orderBy('p.surname')
+                                        ->addOrderBy('p.firstName')
+                                        ;
+                                },
+                            ],
+                        ],
+                    ],
+                    'panel' => 'Expenses',
+                ]
+            )
+            ->add('submit5', SubmitType::class,
+                [
+                    'label' => 'Submit',
+                    'panel' => 'Expenses',
+                    'translation_domain' => 'messages',
+                ]
+            )
+        ;
+    }
+
+    /**
+     * configureOptions
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => null,
+                'translation_domain' => 'SchoolAdmin',
+            ]
+        );
+    }
+
+    /**
+     * getParent
+     * @return string|null
+     */
+    public function getParent()
+    {
+        return ReactFormType::class;
+    }
+
+    /**
+     * getNameStyleList
+     * @return array
+     */
+    public static function getNameStyleList(): array
+    {
+        return [
+            'Surname, Preferred Name',
+            'Official Name',
+        ];
+    }
+
+    /**
+     * getInvoiceNumberList
+     * @return array
+     */
+    public static function getInvoiceNumberList(): array
+    {
+        return [
+            'Invoice ID',
+            'Person ID + Invoice ID',
+            'Student ID + Invoice ID',
+        ];
+    }
+
+    /**
+     * getApprovalList
+     * @return array
+     */
+    public static function getApprovalList(): array
+    {
+        return [
+            'One Of','Two Of','Chain Of All'
+        ];
+    }
+}
